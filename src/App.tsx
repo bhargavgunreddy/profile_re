22e4acb7145433ccefa22d0d1bf3c8b4930ed710
bhargavgunreddy <gbhargavreddy@gmail.com>
2020-06-09 12:02:36 -0400

    [172773536] || Add day night mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
 import { Header } from './features/header/header';
 import Rating from './features/rating/rating';
+import ThemeSwitch from './features/theme/themeSwitch';
 
 function App() {
+  
+  const [state, setState] = useState({'theme':'day'});
+
+  const handleThemeChange = (params:any) => {
+    setState({'theme': params});
+
+  }
   return (
     <div className="App">
+      <ThemeSwitch/>
       <Rating />  
       <img src={logo} className="App-logo" alt="logo" />
         <p>
